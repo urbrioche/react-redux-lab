d5903b1fec0352f9512be6ed6635353428cc23d0
jerry <urbrioche@gmail.com>
2022-07-03 20:56:56 +0800

    [chapter6] apiSlice TodoList component

---
+++ chapter-06-rtk-query/src/features/api/apiSlice.ts
@@ -0,0 +1,45 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import {Todo} from "../../types/Todo";
+
+// typeScript reference: https://redux-toolkit.js.org/rtk-query/usage-with-typescript#createapi
+export const apiSlice = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3500'}),
+    tagTypes: ['Todos'],
+    endpoints: (builder) => ({
+        getTodos: builder.query<Todo[], void>({
+            query: () => `/todos`,
+            providesTags: ['Todos'],
+        }),
+        addTodo: builder.mutation<void, Omit<Todo, "id">>({
+            query: (todo: Omit<Todo, "id">) => ({
+                url: '/todos',
+                method: 'POST',
+                body: todo
+            }),
+            invalidatesTags: ['Todos'],
+        }),
+        updateTodo: builder.mutation<void, Todo>({
+            query: (todo: Todo) => ({
+                url: `/todos/${todo.id}`,
+                method: 'PUT',
+                body: todo
+            }),
+            invalidatesTags: ['Todos'],
+        }),
+        deleteTodo: builder.mutation<void, Pick<Todo, "id">>({
+            query: ({id}: Pick<Todo, "id">) => ({
+                url: `/todos/${id}`,
+                method: 'DELETE',
+                body: id
+            }),
+            invalidatesTags: ['Todos'],
+        }),
+    }),
+});
+export const {
+    useGetTodosQuery,
+    useAddTodoMutation,
+    useUpdateTodoMutation,
+    useDeleteTodoMutation
+} = apiSlice;
--- chapter-06-rtk-query/src/index.tsx
+++ chapter-06-rtk-query/src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApiProvider} from "@reduxjs/toolkit/dist/query/react";
+import {apiSlice} from "./features/api/apiSlice";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ApiProvider api={apiSlice}>
+            <App/>
+        </ApiProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
